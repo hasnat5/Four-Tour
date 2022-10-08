95167de9bd69d0dd549d74343d0af80a7ab9b727
hasnat5 <hasnatferdiananda5@gmail.com>
2022-10-08 22:07:54 +0700

    adding animation

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function navbar() {
 
     const collapse_navbar = document.querySelector(".navbar-collapse")
-    
+
     window.onclick = e => {
-        if ( e.target != collapse_navbar && collapse_navbar.classList.contains("show")) {
-            
+        if (e.target != collapse_navbar && collapse_navbar.classList.contains("show")) {
+
             collapse_navbar.classList.remove("show")
-    
+
         }
     }
 }
